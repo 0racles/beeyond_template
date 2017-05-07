28a132f6c0dc13125743598f5d357f5f50db4d22
UG-Longe <Dare.Longe@city.ac.uk>
2017-05-07 21:53:01 +0100

    adding directives and controllers

---
+++ scripts/directive/infoButton.js
@@ -0,0 +1,20 @@
+app.directive('infoButton', function () {
+   return {
+      restrict: 'E',
+	  scope: {
+		  source: '='
+	  },
+	  templateUrl: 'scripts/directive/infoButton.html',
+	  link : function (scope, element, attr) {
+		  //scope.showMe = false;
+		  scope.display = function () {
+			  scope.showMe = !scope.showMe;
+			  if (!scope.showMe) {
+				  scope.title = "Hide";
+			  } else {
+				  scope.title = "show";
+			  }
+		  }
+	  }
+   };
+});
